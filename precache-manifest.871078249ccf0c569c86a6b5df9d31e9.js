eaff62ed62ff4cddf505a35739122e19a360c9d1
Ngoc Nhi <ngocnhinguyen1001@gmail.com>
2021-01-07 02:19:33 +0700

    Updates

---
+++ precache-manifest.871078249ccf0c569c86a6b5df9d31e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9d7ae14f50a69c04dcc4dfee2bc08b",
+    "url": "/react-habits/index.html"
+  },
+  {
+    "revision": "83b88403fbf7aa6c13a0",
+    "url": "/react-habits/static/css/main.fad9b11d.chunk.css"
+  },
+  {
+    "revision": "a7e75fa83d1d584b6694",
+    "url": "/react-habits/static/js/2.4825573e.chunk.js"
+  },
+  {
+    "revision": "4d8c58c763eb6f656d38d13317751fe9",
+    "url": "/react-habits/static/js/2.4825573e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83b88403fbf7aa6c13a0",
+    "url": "/react-habits/static/js/main.aedf1e94.chunk.js"
+  },
+  {
+    "revision": "9af7812add3a77a717f3",
+    "url": "/react-habits/static/js/runtime-main.474a0b06.js"
+  }
+]);
